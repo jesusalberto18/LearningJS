f0a86a8fd8922c416f58e6f6c3867c6ccb42167c
Alberto <albertiae.digital@gmail.com>
2023-01-01 12:25:55 -0400

    Created for_in.js

---
+++ js_loops/for_in.js
@@ -0,0 +1,9 @@
+// for in
+
+let arrayFruit = ['banana', 'apple', 'orange',
+                   'mango', 'grape', 'kiwi', 'strawberry'];
+
+for (fruit in arrayFruit) {
+  document.write(fruit + '<br>');
+  document.write('<br>');
+}
