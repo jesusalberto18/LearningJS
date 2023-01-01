5f58b70be2e09e3cdab074994a7fbda5fd0b4171
Alberto <albertiae.digital@gmail.com>
2023-01-01 12:26:12 -0400

    Created for_of.js

---
+++ js_loops/for_of.js
@@ -0,0 +1,9 @@
+// for of
+
+let arrayColor = ['red', 'blue', 'green',
+                   'yellow', 'purple', 'black', 'white'];
+
+for (color of arrayColor) {
+  document.write(color);
+  document.write('<br>');
+}
