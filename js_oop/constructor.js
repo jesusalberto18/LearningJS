a2a1e613c0435eca23484c601aa29bf2a75e2b49
Alberto <albertiae.digital@gmail.com>
2023-01-01 12:10:18 -0400

    Created constructor.js

---
+++ js_oop/constructor.js
@@ -0,0 +1,15 @@
+// constructor
+
+class Animal {
+	constructor(type, species, age) {
+		this.type = type;
+		this.species = species;
+		this.age = age;
+	}
+}
+
+const dog = new animal("mammal", "dog", 5);
+
+console.log(animal);
+
+document.write(dog.type);
