f2e7a8356e003009d1730647704e7b8251dfbaf6
Alberto <albertiae.digital@gmail.com>
2023-01-01 12:18:15 -0400

    Modified functions.js

--- js_functions/functions.js
+++ js_functions/functions.js
@@ -2,3 +2,13 @@
 function myFunction(p1, p2) {
   return p1 * p2;
 }
+myFunction();
+
+// Function to greet someone
+
+function greet() {
+  name = prompt("What's your name?");
+  greet = document.write(`Hello ${name}!`);
+  return greet;
+}
+greet();
