d16028ad26c9f026a4d95cc2d7fb0ddb63b3a5eb
Alberto <albertiae.digital@gmail.com>
2023-01-01 12:30:36 -0400

    Created concat_htmlLike.js

---
+++ js_concat/concat_htmlLike.js
@@ -0,0 +1,10 @@
+// concat ${}
+
+let name = 'J.A. Hernández';
+let htmlLike = `<div style="color:red; background:darkblue;">
+                  <h1>
+                    Hello World
+                  </h1>
+                  <h3>By ${name}</h3>
+                </div>`;
+document.write(htmlLike);
