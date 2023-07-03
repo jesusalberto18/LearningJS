3ac0428c8809dba2d015beb1add316c61b1b95c6
Alberto Hernández <albertiae.digital@gmail.com>
2023-07-02 19:12:04 -0500

    Created iteration.js

--- js_objects/iteration.js
+++ js_objects/iteration.js
@@ -1,1 +1,20 @@
-const obj = {};
+const obj = {2:´two´,
+             1:´one´,
+             4:´four´,
+             ´one´:´alsoOne´};
+
+Object.keys(obj);
+// [¨1¨, ¨2¨, ¨4¨, ¨one¨]
+
+Object.values(obj);
+// [¨one¨, ¨two¨, ¨four¨, ¨alsoOne¨]
+
+Object.entries(obj);
+/*
+              [
+               [2, ´two´,
+                1, ´one´,
+                4, ´four´,
+                ´one´, ´alsoOne´]
+              ]
+*/
